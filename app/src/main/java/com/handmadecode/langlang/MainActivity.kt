a273f1228ae3281a2604f692d94681c928313ffc
unknown <woo00916@gmail.com>
2019-03-30 15:51:28 +0900

    deleted data class

--- app/src/main/java/com/handmadecode/langlang/MainActivity.kt
+++ app/src/main/java/com/handmadecode/langlang/MainActivity.kt
@@ -8,7 +8,6 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
 import kotlinx.android.synthetic.main.activity_main.*
-data class {}
 
 class MainActivity : AppCompatActivity() {
     //language setting
